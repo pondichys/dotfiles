f0e7d43e532acd02268537fd90e640060a7dfabf
Sébastien Pondichy <sebastien.pondichy@stib.brussels>
2024-02-23 09:51:06 +0100

    feat(fish): update config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,3 +1,41 @@
+### EXPORT ###
+set -e fish_greeting					# Suppresses fish's greeting message
+set EDITOR "nvim"
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+### FUNCTIONS ###
+function getakscfg -a cluster_name -d "Get kubeconfig file for a specific AKS cluster. You need to be logged in az cli!"
+	az aks get-credentials -g rg-aks-$cluster_name-prd -n aks-$cluster_name-prd --admin
+end
+
+### ALIASES ###
+# Changing ls to lsd
+alias ls='lsd -al --color=always --group-directories-first'
+alias la='lsd -a --color=always --group-directories-first'
+alias l.='lsd -a | rg "^\."'
+
+# Better defaults
+alias df='df -h'
+alias free='free -m'
+
+# git
+alias gst='git status'
+alias lg='lazygit'
+
+# kubernetes
+alias k='kubectl'
+alias kgp='kubectl get pods'
+
+# nvim
+alias v='nvim'
+alias vi='nvim'
+alias vim='nvim'
+
+### zoxide
+zoxide init fish | source
+
+### starship
+starship init fish | source
