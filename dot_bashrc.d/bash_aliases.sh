e27b2c9808c3dc38b218e4e690f186e55d6ec20d
Sébastien Pondichy <sebastien.pondichy@gmail.com>
2024-11-19 21:26:41 +0100

    feat(bash): add aliases for pacman

--- dot_bashrc.d/bash_aliases.sh
+++ dot_bashrc.d/bash_aliases.sh
@@ -1,13 +1,14 @@
+#!/usr/bin/env bash
+
 alias cat='bat'
 
 alias v='nvim'
 alias vi='nvim'
-alias vim='nvim'
 
 # Kubernetes stuff
-alias k='kubectl'
-alias kc='kubectx'
-alias kns='kubens'
+# alias k='kubectl'
+# alias kc='kubectx'
+# alias kns='kubens'
 
 # File stuff
 if command -v lsd &> /dev/null
@@ -43,7 +44,10 @@ alias gp='git push'
 alias glgg='git log --graph'
 alias glgga='git log --graph --decorate --all'
 alias glo='git log --pretty="oneline"'
-alias lg='lazygit'
+if command -v lazygit &> /dev/null
+then
+	alias lg='lazygit'
+fi
 
 # Top 5 processes consuming memory
 alias psmem='ps auxf | sort -nr -k 4 | head -5'
@@ -57,5 +61,51 @@ alias fix_flatpak='fix-flatpak'
 alias fix_flatpaks='fix-flatpak'
 
 # Update GRUB
-alias update-grub='grub-mkconfig -o /boot/grub/grub.cfg'
-alias grub-update='grub-mkconfig -o /boot/grub/grub.cfg'
+alias update-grub='sudo grub-mkconfig -o /boot/grub/grub.cfg'
+alias grub-update='sudo grub-mkconfig -o /boot/grub/grub.cfg'
+
+# PACMAN
+# show the list of packages that need this package - depends mpv as example
+function_depends()  {
+	search=$(echo "$1")
+	sudo pacman -Sii $search | grep "Required" | sed -e "s/Required By     : //g" | sed -e "s/  /\n/g"
+}
+
+alias depends="function_depends"
+
+# pacman unlock
+alias unlock="sudo rm /var/lib/pacman/db.lck"
+alias rmpacmanlock="sudo rm /var/lib/pacman/db.lck"
+
+# Aliases for software managment
+# pacman
+alias update="sudo pacman -Syyu"
+alias upd="sudo pacman -Syyu"
+
+# Recent Installed Packages
+alias rip="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -200 | nl"
+alias riplong="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -3000 | nl"
+
+# Cleanup orphaned packages
+alias cleanup="sudo pacman -Rns $(pacman -Qtdq)"
+
+# Get the error messages from journalctl
+alias jctl="journalctl -p 3 -xb"
+
+# Various fixes
+function_fixkeys() {
+	sudo rm /var/lib/pacman/sync/* && \
+	sudo rm -rf /etc/pacman.d/gnupg/* && \
+	sudo pacman-key --init && \
+	sudo pacman-key --populate && \	
+	sudo pacman -Sy --noconfirm archlinux-keyring && \
+	sudo pacman --noconfirm -Su
+}
+
+alias keyfix="function_fixkeys"
+alias key-fix="function_fixkeys"
+alias keys-fix="function_fixkeys"
+alias fixkey="function_fixkeys"
+alias fixkeys="function_fixkeys"
+alias fix-key="function_fixkeys"
+alias fix-keys="function_fixkeys"
