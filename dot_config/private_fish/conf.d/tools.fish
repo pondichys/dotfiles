8a2f149165b2592f67759a3d6d9dfafd9361c666
Sébastien Pondichy <sebastien.pondichy@gmail.com>
2025-06-06 11:12:21 +0200

    chore(fish): refactor config files

---
+++ dot_config/private_fish/conf.d/tools.fish
@@ -0,0 +1,43 @@
+#!/usr/bin/env fish
+# 
+### homebrew for linux
+if test -d /home/linuxbrew
+    eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+end
+
+if status is-interactive
+
+    ### FUNCTIONS ###
+    # Fix bug with flatpak applications that cannot start when keybase is installed and /kbfs is mounted
+    # https://github.com/flatpak/flatpak/issues/5496
+    function fix-flatpak -d "Fix flatpak startup issue"
+        systemctl --user restart keybase-redirector.service kbfs.service
+    end
+
+    ### Activate mise
+    if type -q mise
+        mise activate fishi --shims | source
+    end
+
+    ### zoxide
+    if type -q zoxide
+        zoxide init fish | source
+        alias za="zoxide add $PWD"
+    end
+
+    ### fastfetch
+    if type -q fastfetch
+        alias ff='fastfetch'
+        fastfetch
+    end
+
+    ### fzf
+    if type -q fzf
+        fzf --fish | source
+    end
+
+    ### starship
+    if type -q starship
+        starship init fish | source
+    end
+end
