46f0a7755c4200768f7307d87506ae9ac1ccf2dc
Sébastien Pondichy <sebastien.pondichy@gmail.com>
2025-07-07 21:06:14 +0200

    feat(qtile): add wallpaper

--- dot_config/qtile/config.py
+++ dot_config/qtile/config.py
@@ -203,6 +203,8 @@ screens = [
             # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
             # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
         ),
+        wallpaper=Path.home() / "git/wallpapers/wallhaven-qz2e8l.jpg",
+        wallpaper_mode="fill",
         # You can uncomment this variable if you see that on X11 floating resize/moving is laggy
         # By default we handle these events delayed to already improve performance, however your system might still be struggling
         # This variable is set to None (no cap) by default, but you can set it to 60 to indicate that you limit it to 60 events per second
